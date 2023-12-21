001fd30380b6ad214244288dc0f917e39ed840ec
Frank Schilling <fschilling@techfak.uni-bielefeld.de>
2023-12-21 20:47:40 +0100

    added feature to start analyzing gaze starting at a specific timestamp in video

--- MCGaze_demo/ExtractFeatures.py
+++ MCGaze_demo/ExtractFeatures.py
@@ -13,6 +13,14 @@ def parse_args():
         )
     
     parser.add_argument(
+        '--timestamp-to-start-at',
+        dest='timestamp_to_start_at',
+        help='point in time of the video from which on gaze shall be estimated (in seconds)',  
+        type=float,
+        default=0.0
+    )
+    
+    parser.add_argument(
         '-v',
         help='visualize output (creates a new video with estimated gaze)',
         action='store_true'
@@ -30,4 +38,4 @@ if __name__ == '__main__':
         exit()
 
     os.system('python3 head_det.py --video {}'.format(args.video_path))
-    os.system('python3 demo.py{}'.format(' -v' if args.v else ''))
+    os.system('python3 demo.py {}{}'.format(args.timestamp_to_start_at, ' -v' if args.v else ''))
